9c4cbd3f6071dc9bbf4d4de6319ce65e234e8f25
Kema-dev <jeremy.jdn@free.fr>
2021-04-21 10:49:00 +0200

    wtf

--- setup.sh
+++ setup.sh
@@ -2,14 +2,13 @@
 
 function build()
 {
-    eval $(minikube docker-env)
     docker build -f srcs/$1/$1.dockerfile -t my-$1 ./srcs/$1/
     kubectl apply -f srcs/$1/$1.yaml
 }
 
 function kube()
 {
-	minikube delete
+	#minikube delete
 	minikube start
 	IP_MINIKUBE=$(minikube ip)
 
