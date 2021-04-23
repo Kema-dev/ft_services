2a29b5a4cfa3a64d876e43f0e11baca1c5e3f922
Kema-dev <jeremy.jdn@free.fr>
2021-04-23 12:07:36 +0200

    wtf

--- setup.sh
+++ setup.sh
@@ -13,6 +13,12 @@ function kube()
 	minikube start --driver=virtualbox
 	IP_MINIKUBE=$(minikube ip)
 
+	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/namespace.yaml
+	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.6/manifests/metallb.yaml
+	kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+	sed -e "s/MINIKUBE_IP/$IP_MINIKUBE/g" srcs/metallb/bare_metallb.yaml > srcs/metallb/metallb.yaml
+	kubectl apply -f srcs/metallb/metallb.yaml
+
 	kubectl apply -f srcs/mysql/storage_class.yaml
 	build mysql
 	build nginx
